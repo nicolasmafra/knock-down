41398cba0640fb902fca2fb281a36b0f210b4cf9
Nicolas Mafra <nickmafra@hotmail.com>
2023-04-13 00:33:45 -0300

    feat: add relative mouse

--- gamepad-keyboard.js
+++ gamepad-keyboard.js
@@ -27,6 +27,8 @@ const gamepadKeyboard = {
 		axes: [0,0],
 	},
 	mouseState: null,
+	isMouseRelative: true,
+	mouseStartPosition: [0,0],
 	
 	getGamepad: function() {
 		return this.state.buttons.length == 0 ? null : this.state;
@@ -72,16 +74,23 @@ const gamepadKeyboard = {
 	onMouseStateChange: function(event) {
 		var flags = event.buttons !== undefined ? event.buttons : event.which;
 		this.mouseState = (flags & 1) === 1;
+		this.mouseStartPosition = [ event.pageX, event.pageY ];
 		this.onmousemove(event);
 	},
 	
 	onmousemove: function(event) {
-		if (this.mouseState) {
-			this.state.axes[0] = -1 + 2* event.pageX / window.innerWidth;
-			this.state.axes[1] = -1 + 2* event.pageY / window.innerHeight;
-		} else {
+		if (!this.mouseState) {
 			this.state.axes[0] = 0;
 			this.state.axes[1] = 0;
+			return;
+		}
+		let size = Math.min(window.innerWidth, window.innerHeight);
+		if (this.isMouseRelative) {
+			this.state.axes[0] = 2*(event.pageX - this.mouseStartPosition[0]) / size;
+			this.state.axes[1] = 2*(event.pageY - this.mouseStartPosition[1]) / size;
+		} else {
+			this.state.axes[0] = -1 + 2* event.pageX / size;
+			this.state.axes[1] = -1 + 2* event.pageY / size;
 		}
 	},
 };
