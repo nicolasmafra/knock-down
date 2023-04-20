5a5e44725267a5b32e2bd84f256d9c705d0a9916
Nicolas Mafra <nickmafra@hotmail.com>
2023-04-19 22:40:10 -0300

    feat: add three JS

--- public/game/game.js
+++ public/game/game.js
@@ -1,28 +1,48 @@
 import { gamepadKeyboard } from '../libs/gamepad-keyboard.js';
 import { gamepadProxy } from '../libs/gamepad-proxy.js';
 import looper from '../libs/looper.js';
+import * as THREE from 'three';
 
 const game = {
 
+	container: document.getElementsByClassName('game-container')[0],
 	fpsElement: document.getElementById('FPS'),
-	boxElement: document.getElementById('box'),
 
-	boxPosition: [0,0],
+	renderer: new THREE.WebGLRenderer(),
+	scene: new THREE.Scene(),
+	camera: new THREE.PerspectiveCamera(),
+
+	box: null,
 	boxMove: [0,0],
-	boxSpeed: 3,
+	boxSpeed: 0.1,
 
 	configure: function() {
 		gamepadKeyboard.configure();
 		gamepadProxy.additionalGamepads.push(gamepadKeyboard.getGamepad());
 		looper.saveFpsHistory = true;
 		looper.renderFunction = (delta) => this.render(delta);
+
+		this.resize();
+		window.onresize = () => this.resize();
+
+		this.container.appendChild(this.renderer.domElement);
+	},
+
+	resize: function() {
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+		this.camera.aspect = window.innerWidth / window.innerHeight;
+		this.camera.updateProjectionMatrix();
 	},
 
 	start: function() {
-		this.boxPosition = [
-			window.innerWidth/2,
-			window.innerHeight/2,
-		];
+		const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+		const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+		this.box = new THREE.Mesh(geometry, material);
+		this.scene.add(this.box);
+
+		this.scene.background = new THREE.Color("green");
+		this.camera.position.z = 5;
 		
 		looper.start();
 	},
@@ -30,6 +50,7 @@ const game = {
 	render: function(delta) {
 		this.updateGui();
 		this.moveBox();
+		this.renderer.render(this.scene, this.camera);
 	},
 
 	updateGui: function() {
@@ -45,15 +66,12 @@ const game = {
 		const gamepad = gamepadProxy.getGamepads()[0];
 		if (gamepad) {
 			this.boxMove[0] += gamepad.buttons[15].value - gamepad.buttons[14].value + gamepad.axes[0];
-			this.boxMove[1] += gamepad.buttons[13].value - gamepad.buttons[12].value + gamepad.axes[1];
+			this.boxMove[1] -= gamepad.buttons[13].value - gamepad.buttons[12].value + gamepad.axes[1];
 			
 			gamepadProxy.normalizeAxisPair(this.boxMove);
 			
-			this.boxPosition[0] += this.boxSpeed * this.boxMove[0];
-			this.boxPosition[1] += this.boxSpeed * this.boxMove[1];
-			
-			this.boxElement.style.left = this.boxPosition[0] + "px";
-			this.boxElement.style.top = this.boxPosition[1] + "px";
+			this.box.position.x += this.boxSpeed * this.boxMove[0];
+			this.box.position.y += this.boxSpeed * this.boxMove[1];
 		}
 	},
 };
