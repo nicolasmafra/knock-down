6c3f79223b5ae1dbc4538c19e939a7d87427da3d
Nicolas Mafra <nickmafra@hotmail.com>
2024-10-26 15:44:15 -0300

    fix: check gamepad button existence

--- public/libs/gamepad-menu.js
+++ public/libs/gamepad-menu.js
@@ -8,19 +8,19 @@ export default {
         
         let inActionNow = false;
         gamepadProxy.getGamepads().forEach(gamepad => {
-            if (gamepad.axes[1] > 0.5 || gamepad.buttons[13].value > 0) {
+            if (gamepad.axes[1] > 0.5 || gamepad.buttons[13] && gamepad.buttons[13].value > 0) {
                 if (!this.inAction) menuLib.moveDown();
                 inActionNow = true;
             }
-            if (gamepad.axes[1] < -0.5 || gamepad.buttons[12].value > 0) {
+            if (gamepad.axes[1] < -0.5 || gamepad.buttons[12] && gamepad.buttons[12].value > 0) {
                 if (!this.inAction) menuLib.moveUp();
                 inActionNow = true;
             }
-            if (gamepad.buttons[0].value > 0) {
+            if (gamepad.buttons[0] && gamepad.buttons[0].value > 0) {
                 if (!this.inAction) menuLib.select();
                 inActionNow = true;
             }
-            if (gamepad.buttons[1].value > 0) {
+            if (gamepad.buttons[1] && gamepad.buttons[1].value > 0) {
                 if (!this.inAction) menuLib.back();
                 inActionNow = true;
             }
